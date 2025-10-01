8099f7ac940af794c3f85f7014fff90f6427003c
Xiangpeng Yang <knightyxp@gmail.com>
2025-10-02 00:49:26 +1000

    new train lora 14b

--- scripts/wan2.1/train_lora_14b.sh
+++ scripts/wan2.1/train_lora_14b.sh
@@ -3,20 +3,7 @@ export DATASET_NAME="/scratch3/yan204/yxp/Senorita"
 export DATASET_META_NAME="/scratch3/yan204/yxp/InContext-VideoEdit/data/json/obj_swap_top1w.json"
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 export NCCL_DEBUG=INFO
-
-# 放本地盘，别用 NFS
 export TRITON_CACHE_DIR=/scratch3/yan204/.triton_cache
-export HF_HOME=/scratch3/yan204/.hf
-export TRANSFORMERS_CACHE=/scratch3/yan204/.hf/transformers
-export TORCH_HOME=/scratch3/yan204/.torch
-
-# 降并行与开销
-export OMP_NUM_THREADS=1
-export MKL_NUM_THREADS=1
-export TOKENIZERS_PARALLELISM=false
-
-# 显存碎片更友好
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 
 accelerate launch \
@@ -33,8 +20,8 @@ accelerate launch \
   --source_frames=33 \
   --edit_frames=32 \
   --train_batch_size=1 \
-  --gradient_accumulation_steps=2 \
-  --dataloader_num_workers=0 \
+  --gradient_accumulation_steps=1 \
+  --dataloader_num_workers=2 \
   --num_train_epochs=2 \
   --checkpointing_steps=500 \
   --learning_rate=1e-04 \
@@ -46,6 +33,7 @@ accelerate launch \
   --adam_epsilon=1e-10 \
   --vae_mini_batch=1 \
   --max_grad_norm=0.05 \
+  --random_hw_adapt \
   --enable_bucket \
   --uniform_sampling \
   --video_edit_loss_on_edited_frames_only \
