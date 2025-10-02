9b7856cb875d5bd17f09ab952e35bff4e3bb77d2
Xiangpeng Yang <knightyxp@gmail.com>
2025-10-03 01:21:20 +1000

    right, start cof

--- scripts/wan2.1/train_reasoning_rem.sh
+++ scripts/wan2.1/train_reasoning_rem.sh
@@ -27,7 +27,7 @@ accelerate launch \
   --checkpointing_steps=500 \
   --learning_rate=1e-04 \
   --seed=42 \
-  --output_dir="experiments/videox_fun_bucket_dynamic_resolution_1.3b_bz32_2epoch_right_init_latents_zero2" \
+  --output_dir="experiments/CoF_ground_then_removal_only_multi_instance_data_zero2" \
   --gradient_checkpointing \
   --mixed_precision="bf16" \
   --adam_weight_decay=3e-2 \
