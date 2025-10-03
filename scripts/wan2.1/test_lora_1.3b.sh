dea22d20fb868f20ecc8afbe2d386a894495a896
Xiangpeng Yang <knightyxp@gmail.com>
2025-10-03 17:02:24 +1000

    test script

--- scripts/wan2.1/test_lora_1.3b.sh
+++ scripts/wan2.1/test_lora_1.3b.sh
@@ -5,4 +5,5 @@ torchrun --nproc_per_node=4 examples/wan2.1/predict_v2v_json.py \
   --output_dir results/chain_of_frames_ground_and_rem_test_in_domain \
   --seed 0 \
   --num_frames 69 \
+  --source_frames 33 \
   --lora_path experiments/CoF_ground_then_removal_only_multi_instance_data_zero2/checkpoint-302.safetensors
